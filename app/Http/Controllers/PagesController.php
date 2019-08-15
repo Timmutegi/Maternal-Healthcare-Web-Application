1323010768b54bb1b0cd3bf0a448f30305ab73ef
Timothy Mutegi <mutegi.timothy538@gmail.com>
2019-08-15 22:09:20 +0300

    Added bar charts and pie charts

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Charts\BCGVaccines;
+use App\Charts\at6weeks;
 use App\Immunization;
 
 class PagesController extends Controller
@@ -17,18 +18,110 @@ class PagesController extends Controller
     }
 
     public function analytics(){
-        $bcg = Immunization::where('bcg_at_birth', '>', 0)->count();
-        $opv = Immunization::where('opv_at_birth', '>', 0)->count();
+        $bcg_at_birth = Immunization::where('bcg_at_birth', '>', 0)->count();
+        $opv_at_birth = Immunization::where('opv_at_birth', '>', 0)->count();
 
-        $chart = new BCGVaccines;
+        $opv_at_6_weeks = Immunization::where('opv_at_6_weeks', '>', 0)->count();
+        $opv_at_10_weeks = Immunization::where('opv_at_10_weeks', '>', 0)->count();
+        $opv_at_14_weeks = Immunization::where('opv_at_14_weeks', '>', 0)->count();
 
-        $chart->labels(['BCG', 'OPV']);
-        $chart->dataset('Vaccines Given', 'column', [$bcg, $opv])->options([
+        $dpt_at_6_weeks = Immunization::where('dpt_at_6_weeks', '>', 0)->count();
+        $dpt_at_10_weeks = Immunization::where('dpt_at_10_weeks', '>', 0)->count();
+        $dpt_at_14_weeks = Immunization::where('dpt_at_14_weeks', '>', 0)->count();
+
+        $pcv_at_6_weeks = Immunization::where('pcv_at_6_weeks', '>', 0)->count();
+        $pcv_at_10_weeks = Immunization::where('pcv_at_10_weeks', '>', 0)->count();
+        $pcv_at_14_weeks = Immunization::where('pcv_at_14_weeks', '>', 0)->count();
+        
+        $yellowfever = Immunization::where('yellow_fever_at_9_months', '>', 0)->count();
+        $measles = Immunization::where('measles_at_9_months', '>', 0)->count();
+
+        // At Birth Analytics
+        $at_birth_chart = new BCGVaccines;
+
+        $at_birth_chart->labels(['BCG', 'POLIO']);
+        $at_birth_chart->dataset('Vaccines Given', 'column', [$bcg_at_birth, $opv_at_birth])->options([
             'color' => '#008080',
         ]);
 
+        $at_birth_pie = new BCGVaccines;
+
+        $at_birth_pie->labels(['BCG', 'POLIO']);
+        $at_birth_pie->dataset('Vaccines Given', 'pie', [$bcg_at_birth, $opv_at_birth])->options([
+            'color' => '#008080',
+        ]);
+
+        // At 6 Weeks Analytics
+        $at_6_chart = new at6weeks;
+        
+        $at_6_chart->labels(['POLIO', 'DPT', 'PCV']);
+        $at_6_chart->dataset('Vaccines Given', 'column', [$opv_at_6_weeks, $dpt_at_6_weeks, $pcv_at_6_weeks])->options([
+            'color' => '#008080',
+        ]);
+
+        $at_6_pie = new at6weeks;
+
+        $at_6_pie->labels(['POLIO', 'DPT', 'PCV']);
+        $at_6_pie->dataset('Vaccines Given', 'pie', [$opv_at_6_weeks, $dpt_at_6_weeks, $pcv_at_6_weeks])->options([
+            'color' => '#008080',
+        ]);
+
+        // At 10 weeks Analytics
+        $at_10_chart = new at6weeks;
+
+        $at_10_chart->labels(['POLIO', 'DPT', 'PCV']);
+        $at_10_chart->dataset('Vaccines Given', 'column', [$opv_at_10_weeks, $dpt_at_10_weeks, $pcv_at_10_weeks])->options([
+            'color' => '#008080',
+        ]);
+
+        $at_10_pie = new at6weeks;
+
+        $at_10_pie->labels(['POLIO', 'DPT', 'PCV']);
+        $at_10_pie->dataset('Vaccines Given', 'pie', [$opv_at_10_weeks, $dpt_at_10_weeks, $pcv_at_10_weeks])->options([
+            'color' => '#008080',
+        ]);
+
+        // Analytics at 14 Weeks
+        $at_14_chart = new at6weeks;
+        $at_14_chart->labels(['POLIO', 'DPT', 'PCV']);
+        $at_14_chart->dataset('Vaccines Given', 'column', [$opv_at_14_weeks, $dpt_at_14_weeks, $pcv_at_14_weeks])->options([
+            'color' => '#008080',
+        ]);
+
+        $at_14_pie = new at6weeks;
+        $at_14_pie->labels(['POLIO', 'DPT', 'PCV']);
+        $at_14_pie->dataset('Vaccines Given', 'pie', [$opv_at_14_weeks, $dpt_at_14_weeks, $pcv_at_14_weeks])->options([
+            'color' => '#008080',
+        ]);
+
+        // Analytics at 9 Months
+        $at_9months_chart = new at6weeks;
+        $at_9months_chart->labels(['Yellow Fever', 'Measles']);
+        $at_9months_chart->dataset('Vaccines Given', 'column', [$yellowfever, $measles])->options([
+            'color' => '#008080',
+        ]);
+
+        $at_9months_pie = new at6weeks;
+        $at_9months_pie->labels(['Yellow Fever', 'Measles']);
+        $at_9months_pie->dataset('Vaccines Given', 'pie', [$yellowfever, $measles])->options([
+            'color' => '#008080',
+        ]);
+
+
+
+        // Rendering of Analytics
         return view('pages.analytics', [
-            'chart' => $chart
+            'chart' => $at_birth_chart,
+            'chart0' => $at_birth_pie,
+            'chart1' => $at_6_chart,
+            'chart2' => $at_10_chart,
+            'chart3' => $at_14_chart,
+            'chart4' => $at_6_pie,
+            'chart5' => $at_10_pie,
+            'chart6' => $at_14_pie,
+            'chart7' => $at_9months_chart,
+            'chart8' => $at_9months_pie,
+
         ]);
     }
 }
--- resources/views/pages/analytics.blade.php
+++ resources/views/pages/analytics.blade.php
@@ -10,12 +10,133 @@
             </div>
         </header><br>
         <div class="chart">
-            <div class="col-md-6">
-            {!! $chart->container() !!}
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            At Birth
+                        </div>
+                        <div class="card-body">
+                            {!! $chart->container() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="card-header">
+                            At Birth
+                        </div>
+                        <div class="card-body">
+                            {!! $chart0->container() !!}
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <div class="row">
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            6 Weeks
+                        </div>
+                        <div class="card-body">
+                            {!! $chart1->container() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            6 Weeks
+                        </div>
+                        <div class="card-body">
+                            {!! $chart4->container() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            10 Weeks
+                        </div>
+                        <div class="card-body">
+                            {!! $chart2->container() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            10 Weeks
+                        </div>
+                        <div class="card-body">
+                            {!! $chart5->container() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            14 Weeks
+                        </div>
+                        <div class="card-body">
+                            {!! $chart3->container() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            14 Weeks
+                        </div>
+                        <div class="card-body">
+                            {!! $chart6->container() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            9 Months
+                        </div>
+                        <div class="card-body">
+                            {!! $chart7->container() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class=col-md-6>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            9 Months
+                        </div>
+                        <div class="card-body">
+                            {!! $chart8->container() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.6/highcharts.js" charset="utf-8"></script>
      {!! $chart->script() !!}
+     {!! $chart0->script() !!}
+     {!! $chart1->script() !!}
+     {!! $chart2->script() !!}
+     {!! $chart3->script() !!}
+     {!! $chart4->script() !!}
+     {!! $chart5->script() !!}
+     {!! $chart6->script() !!}
+     {!! $chart7->script() !!}
+     {!! $chart8->script() !!}
 </body>
 @endsection()
